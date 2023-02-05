d1f2613bba38d70ecca9dfe8be94de448cca4c39
Kassir322 <sentsov@sfedu.ru>
2023-02-05 12:57:21 +0300

    login и refresh работают.

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,19 +1,35 @@
 import { observer } from 'mobx-react-lite'
-import React, { FC, useContext } from 'react'
+import React, { FC, useContext, useEffect } from 'react'
+import { Context } from '..'
 import AppWrapper from './AppWrapper'
 import LoginForm from './loginForm/LoginForm'
 import NewTaskForm from './NewTaskForm/NewTaskForm'
 import TaskInfo from './TaskInfo'
 
 const App: FC = () => {
-	return (
-		<>
-			{/* <AppWrapper />
-			<NewTaskForm />
-			<TaskInfo /> */}
-			<LoginForm />
-		</>
-	)
+	const { store } = useContext(Context)
+
+	useEffect(() => {
+		if (localStorage.getItem('token')) {
+			store.checkAuth().then(() => store.setLoading(false))
+		} else {
+			store.setLoading(false)
+		}
+	}, [])
+
+	if (store.isLoading) {
+		return <div>Загрузка...</div>
+	} else if (!store.isAuth) {
+		return <LoginForm />
+	} else {
+		return (
+			<>
+				<AppWrapper />
+				<NewTaskForm />
+				<TaskInfo />
+			</>
+		)
+	}
 }
 
 export default observer(App)
